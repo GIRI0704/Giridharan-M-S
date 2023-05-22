bc5c52f1b265d84b931adf69e38a6a6bc78d392e
GIRI0704 <134040476+GIRI0704@users.noreply.github.com>
2023-05-22 15:07:55 +0530

    Added solution - LeetHub

---
+++ Subset II - GFG/subset-ii.java
@@ -0,0 +1,75 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        PrintWriter ot = new PrintWriter(System.out);
+        int t;
+        t = sc.nextInt();
+        while (t-- > 0) {
+            int n;
+            n = sc.nextInt();
+            int[] nums = new int[n];
+            for (int i = 0; i < n; i++) nums[i] = sc.nextInt();
+
+            Solution obj = new Solution();
+            ArrayList<ArrayList<Integer>> ans = obj.printUniqueSubsets(nums);
+            Collections.sort(ans, new Comparator<ArrayList<Integer>>() {
+                @Override   public int compare(ArrayList<Integer> a,
+                                             ArrayList<Integer> b) {
+                    int al = a.size();
+                    int bl = b.size();
+                    int min = Math.min(al, bl);
+                    for (int i = 0; i < min; i++) {
+                        if (a.get(i) < b.get(i))
+                            return -1;
+                        else if (a.get(i) > b.get(i))
+                            return 1;
+                    }
+                    if (al < bl)
+                        return -1;
+                    else if (al > bl)
+                        return 1;
+                    return -1;
+                }
+            });
+            ot.print("[ ");
+            for (int i = 0; i < ans.size(); i++) {
+                ot.print("[ ");
+                for (int j = 0; j < ans.get(i).size(); j++)
+                    ot.print(ans.get(i).get(j) + " ");
+                ot.print("]");
+            }
+            ot.println(" ]");
+        }
+        ot.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public static ArrayList<ArrayList<Integer>> printUniqueSubsets(int[] nums) {
+        Arrays.sort(nums);
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        fun(nums,0,list,new ArrayList<>());
+        return list;
+    }
+    public static void fun(int arr[], int index, ArrayList<ArrayList<Integer>> list, ArrayList<Integer> ans)
+    {
+        list.add(new ArrayList<>(ans));
+        for(int i = index; i < arr.length; i++)
+        {
+            if(i > index && arr[i] == arr[i-1]) continue;
+            ans.add(arr[i]);
+            fun(arr, i+1, list, ans);
+            ans.remove(ans.size()-1);
+        }
+    }
+}
