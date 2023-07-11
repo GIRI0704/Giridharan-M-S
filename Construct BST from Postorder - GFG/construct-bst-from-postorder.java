d0b1d9d705516f1086b35b3450ceed848d934751
GIRI0704 <134040476+GIRI0704@users.noreply.github.com>
2023-07-11 14:00:50 +0530

    Added solution - LeetHub

---
+++ Construct BST from Postorder - GFG/construct-bst-from-postorder.java
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.Scanner;
+import java.lang.Math;
+import java.io.*;
+class Node{
+    int data;
+    Node left,right;
+    Node(int d)
+    {
+        data=d;
+        left=right=null;
+    }
+}
+class GFG2
+{
+    public static void inorder(Node root)
+    {
+        if(root==null)
+        return;
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0)
+        {
+            int n=sc.nextInt();
+            int []post=new int[n];
+            for(int i=0;i<n;i++)
+            {
+                int b=sc.nextInt();
+                post[i]=b;
+            }
+            GFG obj=new GFG();
+            Node root=obj.constructTree(post,n);
+            inorder(root);
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+/*class Node{
+    int data;
+    Node left,right;
+    Node(int d)
+    {
+        data=d;
+        left=right=null;
+    }
+}*/
+
+// Function should return the root of the bst formed using postorder traversal.
+class GFG
+{
+    public static int index;
+    public static Node constructTree(int post[],int n)
+    {
+        //Add your code here.
+        index = n-1;
+        return counstruct(post, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+    public static Node counstruct(int arr[], int left, int right)
+    {
+        if(index == -1) return null;
+        if(arr[index] < left || arr[index] > right) return null;
+        Node node = new Node(arr[index]);
+        index--;
+        node.right = counstruct(arr, node.data+1, right);
+        node.left = counstruct(arr, left, node.data-1);
+        return node;
+    }
+}
