49167a908e7669b9889941cfdf1c557475f99355
GIRI0704 <134040476+GIRI0704@users.noreply.github.com>
2023-08-08 09:35:14 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (57.22%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -14,16 +14,41 @@
  * }
  */
 class Solution {
-    public List<Integer> postorderTraversal(TreeNode root){
+    public List<Integer> postorderTraversal(TreeNode root) {
+        Stack<TreeNode> s1 = new Stack<>();
+        Stack<TreeNode> s2 = new Stack<>();
+        
         List<Integer> list = new ArrayList<>();
-        fun(root,list);
+        if(root == null) return list;
+        s1.push(root);
+        
+        while(s1.size() > 0)
+        {
+            TreeNode temp = s1.pop();
+            s2.push(temp);
+            if(temp.left != null) s1.push(temp.left);
+            if(temp.right != null) s1.push(temp.right);
+        }
+        
+        while(s2.size() > 0)
+        {
+            list.add(s2.pop().val);
+        }
         return list;
     }
-    public void fun(TreeNode root, List<Integer> list)
-    {
-        if(root == null) return;
-        fun(root.left, list);
-        fun(root.right, list);
-        list.add(root.val);
-    }
 }
+
+// class Solution {
+//     public List<Integer> postorderTraversal(TreeNode root){
+//         List<Integer> list = new ArrayList<>();
+//         fun(root,list);
+//         return list;
+//     }
+//     public void fun(TreeNode root, List<Integer> list)
+//     {
+//         if(root == null) return;
+//         fun(root.left, list);
+//         fun(root.right, list);
+//         list.add(root.val);
+//     }
+// }
