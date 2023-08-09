628200648ce9541b7c7b98925cc7f9afb1a9f906
GIRI0704 <134040476+GIRI0704@users.noreply.github.com>
2023-08-09 13:16:08 +0530

    Time: 1 ms (99.75%), Space: 42.7 MB (93.35%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -25,35 +25,30 @@ class pair
 }
 class Solution {
     public int widthOfBinaryTree(TreeNode root) {
-        if(root == null) return 0;
+        int ans = 0;
         Queue<pair> q = new ArrayDeque<>();
-		int ans = 0;
-		q.add(new pair(root, 0));
-		while(q.size() > 0)
-			{
-				int size = q.size();
-				int first = 0;
-				int last = 0;
-				int min = q.peek().index;
-				for(int i = 0; i < size; i++)
-					{
-						int curr = q.peek().index-min;
-						TreeNode temp = q.peek().node;
-						q.remove();
-						if(i == 0) first = curr;
-						if(i == size-1) last = curr;
-						if(temp.left != null)
-						{
-							q.add(new pair(temp.left, curr*2+1));
-						}
-						if(temp.right != null)
-						{
-							q.add(new pair(temp.right, curr*2+2));
-						}
-					}
-				ans = Math.max(ans, (last-first+1));
-			}
-		return ans;
+        q.add(new pair(root,0));
         
+        while(q.size() > 0)
+        {
+            int first = 0;
+            int last = 0;
+            int min = q.peek().index;
+            int size = q.size();
+            
+            for(int i = 0; i < size; i++)
+            {
+                int curr = q.peek().index-min;
+                TreeNode temp = q.peek().node;
+                q.remove();
+                if(i == 0) first = curr;
+                if(i == size-1) last = curr;
+                
+                if(temp.left != null) q.add(new pair(temp.left, curr*2+1));
+                if(temp.right != null) q.add(new pair(temp.right, curr*2+2));
+            }
+            ans = Math.max(ans, (last-first+1));
+        }
+        return ans;
     }
 }
