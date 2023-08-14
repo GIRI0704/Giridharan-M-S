792492587d3815ea2ab0e0b7a313a9a9d9c3fbad
GIRI0704 <134040476+GIRI0704@users.noreply.github.com>
2023-08-14 12:39:37 +0530

    Time: 0 ms (100.00%), Space: 45.2 MB (61.04%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -14,33 +14,33 @@
  * }
  */
 class Solution {
-    public TreeNode deleteNode(TreeNode root, int key) {
+    public TreeNode deleteNode(TreeNode root, int val) {
         if(root == null) return null;
-        if(key < root.val)
-        {
-            root.left = deleteNode(root.left, key);
-        }
-        else if(key > root.val)
-        {
-            root.right = deleteNode(root.right, key);
-        }
-        else
-        {
-            if(root.left == null && root.right == null) return null;
-            else if(root.left != null && root.right == null) return root.left;
-            else if(root.left == null && root.right != null) return root.right;
-            else
-            {
-                int minval = min(root.right);
-                root.val = minval;
-                root.right = deleteNode(root.right, minval);
-            }
-        }
-        return root;
+		if(val < root.val)
+		{
+			root.left = deleteNode(root.left, val);
+		}
+		else if(val > root.val)
+		{
+			root.right = deleteNode(root.right, val);
+		}
+		else
+		{
+			if(root.left == null && root.right == null) return null;
+			else if(root.left != null && root.right == null) return root.left;
+			else if(root.left == null && root.right != null) return root.right;
+			else
+			{
+				int minval = min(root.right);
+				root.val = minval;
+				root.right = deleteNode(root.right,minval);
+			}
+		}
+		return root;
     }
     public int min(TreeNode root)
     {
-        while(root.left != null) root = root.left;
-		return root.val;
+       while(root.left != null) root = root.left;
+        return root.val;
     }
 }
