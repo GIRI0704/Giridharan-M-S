892922f44719334da2bfd4b91cecf76ab59f88c2
GIRI0704 <134040476+GIRI0704@users.noreply.github.com>
2023-08-18 13:27:54 +0530

    Added solution - LeetHub

--- Rotten Oranges - GFG/rotten-oranges.java
+++ Rotten Oranges - GFG/rotten-oranges.java
@@ -28,73 +28,72 @@ class GFG
 }
 // } Driver Code Ends
 
-
+class pair
+{
+    int row;
+    int column;
+    int time;
+    
+    pair(int row, int column, int time)
+    {
+        this.row = row;
+        this.column = column;
+        this.time = time;
+    }
+}
 class Solution
 {
     //Function to find minimum time required to rot all oranges. 
     public int orangesRotting(int[][] grid)
     {
         // Code here
+        int time = 0;
+        int fresh = 0;
         int n = grid.length;
-		 int m = grid[0].length;
-		 int visited[][] = new int[n][m];
-		 int freshcount = 0;
-		 Queue<pair> q = new ArrayDeque<>();
-		 for(int i = 0; i < n; i++)
-			 {
-				 for(int j = 0; j < m; j++)
-					 {
-						if(grid[i][j] == 2) 
-						{
-							q.add(new pair(i,j,0));
-							visited[i][j] = 2;
-						}
-						else if(grid[i][j] == 1) freshcount++;
-						else visited[i][j] = 0; 
-					 } 
-			 }
-		 int tm = 0;
-		 int count = 0;
-
-		 int delrow[] = {-1,0,1,0};
-		 int delcol[] = {0,1,0,-1};
-		 while(q.size() > 0)
-			 {
-				 int r = q.peek().row;
-				 int c = q.peek().column;
-				 int t = q.peek().time;
-
-				 tm = Math.max(tm, t);
-				 q.remove();
-
-				 for(int i = 0; i < 4; i++)
-					 {
-						 int nrow = r + delrow[i];
-						 int ncolumn = c + delcol[i];
-
-						 if(nrow >= 0 && nrow < n && ncolumn >= 0 && ncolumn < m && visited[nrow][ncolumn] != 2
-						   && grid[nrow][ncolumn] == 1)
-						 {
-							 q.add(new pair(nrow, ncolumn, t+1));
-							 visited[nrow][ncolumn] = 2;
-							 count++;
-						 }
-					 }
-			 }
-		 if(count != freshcount) return -1;
-		 return tm;
+        int m = grid[0].length;
+        Queue<pair> q = new ArrayDeque<>();
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                if(grid[i][j] == 2)
+                {
+                    q.add(new pair(i,j,0));
+                }
+                if(grid[i][j] == 1)
+                {
+                    fresh++;
+                }
+            }
+        }
+        int delrow[] = {-1,0,1,0};
+        int delcol[] = {0,1,0,-1};
+        int count = 0;
+        while(q.size() > 0)
+        {
+            int row = q.peek().row;
+            int col = q.peek().column;
+            int t = q.peek().time;
+            
+            q.remove();
+            
+            time = Math.max(time,t);
+            
+            for(int i = 0; i < 4; i++)
+            {
+                int r = row+delrow[i];
+                int c = col+delcol[i];
+                
+                if(r >= 0 && r < n && c >= 0 && c < m && grid[r][c] == 1)
+                {
+                    count++;
+                    grid[r][c] = 2;
+                    q.add(new pair(r,c,t+1));
+                }
+            }
+        }
+        
+        if(fresh != count) return -1;
+        return time;
     }
 }
-class pair
-	{
-		int row;
-		int column;
-		int time;
-
-		pair(int row, int column, int time)
-		{
-			this.row = row;
-			this.column = column;
-			this.time = time;
-		}
-	}
