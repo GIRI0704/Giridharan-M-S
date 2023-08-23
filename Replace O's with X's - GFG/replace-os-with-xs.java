2025439ee003cba310b540205d3dc6f1459ff3d2
GIRI0704 <134040476+GIRI0704@users.noreply.github.com>
2023-08-23 13:01:43 +0530

    Added solution - LeetHub

--- Replace O's with X's - GFG/replace-os-with-xs.java
+++ Replace O's with X's - GFG/replace-os-with-xs.java
@@ -40,52 +40,50 @@ class GFG{
 //User function Template for Java
 
 class Solution{
-    static void dfs(int row, int column, int delrow[], int delcol[],
-    char grid[][], int visited[][], int n, int m)
+    static void dfs(int r, int c, int n, int m,int delrow[],int delcol[], char a[][], int visited[][])
     {
-        visited[row][column] = 1;
+        visited[r][c] = 1;
+        
         for(int i = 0; i < 4; i++)
         {
-            int r = row + delrow[i];
-            int c = column + delcol[i];
-            if(r >= 0 && r < n && c >= 0 && c < m && visited[r][c] == 0 && grid[r][c] == 'O')
+            int row = r+delrow[i];
+            int column = c+delcol[i];
+            
+            if(row >= 0 && row < n && column >= 0 && column < m && a[row][column]=='O' && visited[row][column]==0)
             {
-                dfs(r,c,delrow,delcol,grid,visited,n,m);
+                dfs(row,column,n,m,delrow,delcol,a,visited);
             }
         }
     }
-    static char[][] fill(int n, int m, char grid[][])
+    static char[][] fill(int n, int m, char a[][])
     {
         // code here
-        n = grid.length;
-        m = grid[0].length;
+        int visited[][] = new int[n][m];
         
-        int visited[][]= new int[n][m];
         int delrow[] = {-1,0,1,0};
         int delcol[] = {0,1,0,-1};
-        for(int j = 0; j < m; j++)
-        {
-            if(grid[0][j] == 'O' && visited[0][j] == 0)
-            {
-                dfs(0,j,delrow,delcol,grid,visited,n,m);
-            }
-            
-            if(grid[n-1][j] == 'O' && visited[n-1][j] == 0)
-            {
-                dfs(n-1,j,delrow,delcol,grid,visited,n,m);
-            }
-        }
         
         for(int i = 0; i < n; i++)
         {
-            if(grid[i][0] == 'O' && visited[i][0] == 0)
+            if(a[i][0] == 'O' && visited[i][0] == 0)
             {
-                dfs(i,0,delrow,delcol,grid,visited,n,m);
+                dfs(i,0,n,m,delrow,delcol,a,visited);
             }
-            
-            if(grid[i][m-1] == 'O' && visited[i][m-1] == 0)
+            if(a[i][m-1] == 'O' && visited[i][m-1] == 0)
             {
-                dfs(i,m-1,delrow,delcol,grid,visited,n,m);
+                dfs(i,m-1,n,m,delrow,delcol,a,visited);
+            }
+        }
+        
+        for(int j = 0; j < m; j++)
+        {
+            if(a[0][j] == 'O' && visited[0][j] == 0)
+            {
+                dfs(0,j,n,m,delrow,delcol,a,visited);
+            }
+            if(a[n-1][j] == 'O' && visited[n-1][j] == 0)
+            {
+                dfs(n-1,j,n,m,delrow,delcol,a,visited);
             }
         }
         
@@ -93,14 +91,13 @@ class Solution{
         {
             for(int j = 0; j < m; j++)
             {
-                if(grid[i][j] == 'O' && visited[i][j] == 0)
+                if(a[i][j] == 'O' && visited[i][j] == 0)
                 {
-                    visited[i][j] = 1;
-                    grid[i][j] = 'X';
+                    a[i][j] = 'X';
                 }
             }
         }
         
-        return grid;
+        return a;
     }
 }
