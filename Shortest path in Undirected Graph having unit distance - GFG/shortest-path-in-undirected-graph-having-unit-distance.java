a4420aacb1abf7f860034fae5430d8a94c37979e
GIRI0704 <134040476+GIRI0704@users.noreply.github.com>
2023-08-25 13:24:34 +0530

    Added solution - LeetHub

--- Shortest path in Undirected Graph having unit distance - GFG/shortest-path-in-undirected-graph-having-unit-distance.java
+++ Shortest path in Undirected Graph having unit distance - GFG/shortest-path-in-undirected-graph-having-unit-distance.java
@@ -40,40 +40,46 @@ class Solution {
             adj.add(new ArrayList<>());
         }
         
-        for(int i = 0; i < m; i++)
+        for(int i = 0 ; i < m; i++)
         {
-            adj.get(edges[i][0]).add(edges[i][1]);
-            adj.get(edges[i][1]).add(edges[i][0]);
+            int v = edges[i][0];
+            int u = edges[i][1];
+            
+            adj.get(v).add(u);
+            adj.get(u).add(v);
         }
         
         int dist[] = new int[n];
+        
         for(int i = 0; i < n; i++)
         {
             dist[i] = (int)1e9;
         }
+        
         dist[src] = 0;
         
         Queue<Integer> q = new ArrayDeque<>();
         q.add(src);
+        
         while(q.size() > 0)
         {
             int node = q.remove();
             
-            for(int i : adj.get(node))
+            for(int it : adj.get(node))
             {
-                if(dist[node]+1 < dist[i])
+                if(dist[node]+1 < dist[it])
                 {
-                    dist[i] = dist[node]+1;
-                    q.add(i);
+                    dist[it] = 1+dist[node];
+                    q.add(it);
                 }
             }
         }
         
-        for(int i = 0; i < n; i++)
+        for(int i = 0 ; i < n;i++)
         {
-            if(dist[i] == 1e9) dist[i] = -1;
+            if(dist[i] == (int)1e9)
+            dist[i] = -1;
         }
-        
         return dist;
     }
 }
