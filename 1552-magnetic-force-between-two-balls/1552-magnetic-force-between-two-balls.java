840f082755904207464ca2396a1ceb6eaf0f8576
GIRI0704 <134040476+GIRI0704@users.noreply.github.com>
2023-09-10 12:43:32 +0530

    Time: 42 ms (94.56%), Space: 54.6 MB (66.10%) - LeetHub

--- 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.java
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.java
@@ -2,14 +2,16 @@ class Solution {
     public int maxDistance(int[] a, int cows) {
         
         Arrays.sort(a);
-		int start = Integer.MAX_VALUE;
+		int start = 1;
 		int end = a[a.length-1] - a[0];
 
-		for(int i = 0; i < a.length-1; i++)
-			{
-				int diff = a[i+1] - a[i];
-				start = Math.min(diff,start);
-			}
+		// for(int i = 0; i < a.length-1; i++)
+		// 	{
+		// 		int diff = a[i+1] - a[i];
+		// 		start = Math.min(diff,start);
+		// 	}
+        
+        // start
 
 		int ans = -1;
 
