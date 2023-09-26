10c0b5bb5f7328660b4f574e554481a92f087033
GIRI0704 <134040476+GIRI0704@users.noreply.github.com>
2023-09-26 10:45:10 +0530

    Time: 75 ms (24.52%), Space: 44.4 MB (52.74%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.java
+++ 0402-remove-k-digits/0402-remove-k-digits.java
@@ -1,35 +1,36 @@
 class Solution {
     public String removeKdigits(String num, int k) {
-        Stack<Character> stack = new Stack<>();
         
-        for (char digit : num.toCharArray()) {
-            // While there are elements in the stack, the current digit is smaller than the top of the stack,
-            // and we can still remove digits (k > 0), pop elements from the stack.
-            while (!stack.isEmpty() && k > 0 && digit < stack.peek()) {
+        Stack<Character> stack = new Stack<>();
+        for(char digit : num.toCharArray())
+        {
+            while(stack.size() > 0 && k > 0 && digit < stack.peek())
+            {
                 stack.pop();
                 k--;
             }
-            // Push the current digit onto the stack.
             stack.push(digit);
         }
         
-        // If there are still remaining digits to be removed (k > 0), pop them from the stack.
-        while (k > 0 && !stack.isEmpty()) {
-            stack.pop();
+        while(k > 0 && stack.size() > 0)
+        {
             k--;
+            stack.pop();
         }
         
-        // Build the result string from the stack.
-        StringBuilder result = new StringBuilder();
-        while (!stack.isEmpty()) {
-            result.insert(0, stack.pop());
+        StringBuilder sb = new StringBuilder();
+        
+        while(stack.size() > 0)
+        {
+            sb.insert(0,stack.pop());
         }
         
-        // Remove leading zeros.
-        while (result.length() > 1 && result.charAt(0) == '0') {
-            result.deleteCharAt(0);
+        while(sb.length() > 0 && sb.charAt(0) == '0')
+        {
+            sb.deleteCharAt(0);
         }
-        if(result.length() == 0) return "0";
-        return result.toString();
+        
+        if(sb.length() == 0) return "0";
+        return sb.toString();
     }
 }
