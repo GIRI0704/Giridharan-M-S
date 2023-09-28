e2160cc745003ea5938a228331709a9f7ecc2c84
GIRI0704 <134040476+GIRI0704@users.noreply.github.com>
2023-09-28 12:37:52 +0530

    Time: 248 ms (5.03%), Space: 44.1 MB (49.93%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,67 +1,50 @@
 class Solution {
-    public String minWindow(String s, String t) {
-        HashMap<Character, Integer> f2 = new HashMap<>();
-        HashMap<Character, Integer> f1 = new HashMap<>();
-
-		for(char ch : t.toCharArray())
+    public boolean freqcheck(HashMap<Character, Integer> f1, HashMap<Character, Integer> f2)
+	{
+		for(char key : f2.keySet())
 			{
-				f2.put(ch, f2.getOrDefault(ch, 0)+1);
+				if(!f1.containsKey(key)) return false;
+				if(f1.get(key) < f2.get(key)) return false;
+			}
+		return true;
+	}
+    public String minWindow(String s, String t) {
+        int left = 0;
+		int right = 0;
+		int len = Integer.MAX_VALUE;
+		String ans = "";
+
+		HashMap<Character, Integer> f2 = new HashMap<>();
+		HashMap<Character, Integer> f1 = new HashMap<>();
+
+		for(int i = 0; i < t.length(); i++)
+			{
+				char ch = t.charAt(i);
+				f2.put(ch, f2.getOrDefault(ch,0)+1);
 			}
 
-		int left = 0;
-		int right = 0;
-        String ans = "";
-		int word2 = t.length();
-		int word1 = 0;
-		while(true)
+		while(right < s.length())
 			{
-				boolean l = false;
-				boolean r = false;
-				if(right < s.length() && word1 < word2)
-				{
-					r = true;
-					char ch = s.charAt(right);
-					right++;
-					if(f2.containsKey(ch))
+				char ch = s.charAt(right);
+				f1.put(ch, f1.getOrDefault(ch, 0)+1);
+
+				while(freqcheck(f1,f2))
 					{
-						if(f1.getOrDefault(ch, 0) < f2.get(ch))
+						if(right-left+1 < len)
 						{
-							word1++;
+							len = right-left+1;
+							ans = s.substring(left,right+1);
 						}
-					}
-					f1.put(ch, f1.getOrDefault(ch,0)+1);
-					
-				}
-				else if(left < right && word1 == word2)
-				{
-					l = true;
-					String substring = s.substring(left, right);
 
-					if(ans == "" || ans.length() > substring.length())
-					{
-						ans = substring;
-					}
-
-					char ch = s.charAt(left);
-
-					if(f2.containsKey(ch))
-					{
-						if(f1.get(ch) <= f2.get(ch))
+						char currleft = s.charAt(left);
+						f1.put(currleft, f1.get(currleft)-1);
+						if(f1.get(currleft) == 0)
 						{
-							word1--;
+							f1.remove(currleft);
 						}
+						left++;
 					}
-
-					f1.put(ch, f1.get(ch)-1);
-
-					if(f1.get(ch) == 0)
-					{
-						f1.remove(ch);
-					}
-
-					left++;
-				}
-				if(l == false && r == false) break;
+				right++;
 			}
 		return ans;
     }
