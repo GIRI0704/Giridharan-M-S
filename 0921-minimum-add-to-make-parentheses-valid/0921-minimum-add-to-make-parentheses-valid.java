8940ffd42bbb32f6bef426ecfc9ae50ce7a39009
GIRI0704 <134040476+GIRI0704@users.noreply.github.com>
2023-09-29 16:19:52 +0530

    Time: 2 ms (49.65%), Space: 40.3 MB (69.94%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        int count = 0;
+        
+        for(int i = 0;i<s.length(); i++)
+        {
+            char ch = s.charAt(i);
+            
+            if(ch == '(')
+            {
+                stack.push(ch);
+            }
+            else
+            {
+                if(stack.size() == 0) count++;
+                
+                if(stack.size()!= 0 && stack.peek() == '(')
+                {
+                    stack.pop();
+                }
+            }
+        }
+        
+    return count + stack.size();
+    }
+}
