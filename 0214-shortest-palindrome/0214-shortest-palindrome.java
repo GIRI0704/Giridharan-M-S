5a26b3a044857f192f6ee8aeb1cbdb02255f875a
GIRI0704 <134040476+GIRI0704@users.noreply.github.com>
2023-09-29 15:59:51 +0530

    Time: 220 ms (19.64%), Space: 44.6 MB (7.90%) - LeetHub

---
+++ 0214-shortest-palindrome/0214-shortest-palindrome.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String shortestPalindrome(String s) {
+        int n = s.length();
+        StringBuilder sb = new StringBuilder(s);
+        String reversed = sb.reverse().toString();
+
+        for (int i = 0; i < n; i++) {
+            if (s.substring(0, n - i).equals(reversed.substring(i))) {
+                return reversed.substring(0, i) + s;
+            }
+        }
+
+        return s;
+    }
+}
