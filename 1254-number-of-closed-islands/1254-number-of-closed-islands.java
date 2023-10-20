03daebb10b512a790d5a25cc610543efea9ea27c
GIRI0704 <134040476+GIRI0704@users.noreply.github.com>
2023-10-20 13:09:03 +0530

    Time: 2 ms (96.93%), Space: 43.1 MB (67.09%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.java
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.java
@@ -1,41 +1,41 @@
 class Solution {
     public int closedIsland(int[][] grid) {
-        if (grid == null || grid.length == 0 || grid[0].length == 0) {
-            return 0;
+        int m = grid.length;
+        int n = grid[0].length;
+
+        for (int j = 0; j < n; j++) {
+            dfs(grid, 0, j);
+            dfs(grid, m - 1, j);
         }
-        int r = grid.length;
-        int c = grid[0].length;
-        int ans = 0;
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (grid[i][j] == 0 && isValidIsland(grid, i, j, r, c)) {
-                    ans++;
+        for (int i = 0; i < m; i++) {
+            dfs(grid, i, 0);
+            dfs(grid, i, n - 1);
+        }
+
+        int res = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 0) {
+                    res++;
+                    dfs(grid, i, j);
                 }
             }
         }
-        return ans;
+        return res;
     }
-    
-    private boolean isValidIsland(int[][] grid, int i, int j, int r, int c) {
-        int[] dx = new int[]{0, 0, 1, -1};
-        int[] dy = new int[]{1, -1, 0, 0};
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{i, j});
-        boolean valid = true;
-        while (!queue.isEmpty()) {
-            int[] node = queue.poll();
-            if (node[0] == 0 || node[0] == r - 1 || node[1] == 0 || node[1] == c - 1) {
-                valid = false;
-            }
-            grid[node[0]][node[1]] = -1;
-            for (int k = 0; k < 4; k++) {
-                int x = node[0] + dx[k];
-                int y = node[1] + dy[k];
-                if (x >= 0 && x < r && y >= 0 && y < c && grid[x][y] == 0) {
-                    queue.offer(new int[]{x, y});
-                }
-            }
+
+    public void dfs(int[][] grid, int i, int j) {
+        int m = grid.length, n = grid[0].length;
+        if (i < 0 || j < 0 || i >= m || j >= n) {
+            return;
         }
-        return valid;
+        if (grid[i][j] == 1) {
+            return;
+        }
+        grid[i][j] = 1;
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j - 1);
     }
 }
