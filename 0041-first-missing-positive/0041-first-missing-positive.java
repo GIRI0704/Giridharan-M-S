34b44072c0cd5925cf2f37ff5e697048ff7b0d00
GIRI0704 <134040476+GIRI0704@users.noreply.github.com>
2023-10-26 11:56:47 +0530

    Time: 1 ms (98.65%), Space: 59.4 MB (25.66%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+
+        // Step 1: Transform the array so that all positive integers are in the range [1, n]
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+
+        // Step 2: Find the first index where the value does not match its index + 1
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        // If all positive integers in [1, n] are present, return n + 1
+        return n + 1;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
