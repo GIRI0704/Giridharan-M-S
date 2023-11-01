bc736d133dfbd4605a2b86690423974d651d81d6
GIRI0704 <134040476+GIRI0704@users.noreply.github.com>
2023-11-01 11:35:04 +0530

    Time: 1 ms (100.00%), Space: 43.9 MB (41.56%) - LeetHub

--- 1367-linked-list-in-binary-tree/1367-linked-list-in-binary-tree.java
+++ 1367-linked-list-in-binary-tree/1367-linked-list-in-binary-tree.java
@@ -25,38 +25,23 @@
  */
 class Solution {
     public boolean isSubPath(ListNode head, TreeNode root) {
-        if (head == null) {
-            // An empty linked list is always considered a subpath.
-            return true;
-        }
-        if (root == null) {
-            // If the binary tree is empty and the linked list is not, it's impossible to find a subpath.
-            return false;
-        }
+        if(head == null) return true;
+        if(root == null) return false;
         
-        // Check if the current node's value matches the head of the linked list.
-        if (head.val == root.val) {
-            // Attempt to find a subpath starting from the current node.
-            if (isSubPathFromCurrentNode(head, root)) {
-                return true;
-            }
+        if(head.val == root.val)
+        {
+            if(fun(head,root)) return true;
         }
+         
+        return isSubPath(head,root.left) || isSubPath(head,root.right);
         
-        // If no subpath was found starting from the current node, continue searching in the left and right subtrees.
-        return isSubPath(head, root.left) || isSubPath(head, root.right);
     }
     
-    private boolean isSubPathFromCurrentNode(ListNode head, TreeNode root) {
-        if (head == null) {
-            // The entire linked list has been matched.
-            return true;
-        }
-        if (root == null || head.val != root.val) {
-            // The current node's value doesn't match the linked list or the binary tree node is null.
-            return false;
-        }
+    public boolean fun(ListNode head, TreeNode root)
+    {
+        if(head == null) return true;
+        if(root == null || root.val != head.val) return false;
         
-        // Continue matching the rest of the linked list in the left and right subtrees.
-        return isSubPathFromCurrentNode(head.next, root.left) || isSubPathFromCurrentNode(head.next, root.right);
+         return fun(head.next,root.left) || fun(head.next,root.right); 
     }
 }
