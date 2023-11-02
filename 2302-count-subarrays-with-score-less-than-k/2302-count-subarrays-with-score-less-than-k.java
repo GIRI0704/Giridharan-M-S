04f44fc23dcc04a9a59ed391a627d89e6dfccb24
GIRI0704 <134040476+GIRI0704@users.noreply.github.com>
2023-11-02 11:33:13 +0530

    Time: 2 ms (100.00%), Space: 57 MB (86.19%) - LeetHub

--- 2302-count-subarrays-with-score-less-than-k/2302-count-subarrays-with-score-less-than-k.java
+++ 2302-count-subarrays-with-score-less-than-k/2302-count-subarrays-with-score-less-than-k.java
@@ -1,19 +1,63 @@
 class Solution {
+//     public long countSubarrays(int[] nums, long k) {
+//         // int start = 0;
+//         // long sum = 0;
+//         // long count = 0;
+//         // for(int end = 0; end < nums.length; end++)
+//         // {
+//         //     sum += nums[end];
+//         //     while((sum * (end-start+1)) >= k)
+//         //     {
+//         //         sum -= nums[start];
+//         //         start++;
+//         //     }
+//         //     if((sum * (end-start+1)) < k)
+//         //         count+=(end-start+1);
+//         // }
+//         // return count;
+        
+//         int s=0;
+// 		int e=0;
+// 		int sum1=0;
+//         int sum2=0;
+// 		long ans=0;
+// 		while(e<nums.length){
+// 		   sum1=sum1+nums[e];
+//            sum2=sum1*(e-s+1);
+// 		   while(sum2>=k ){
+			  
+//                sum1=sum1-nums[s];
+//                sum2=sum1*(e-s+1);
+//          	   s++;
+//          }
+		   
+// 		   ans=ans+(e-s+1);
+// 		   e++;
+		
+// 		}
+		  
+// 		return ans;
+//     }
     public long countSubarrays(int[] nums, long k) {
-        int start = 0;
-        long sum = 0;
-        long count = 0;
-        for(int end = 0; end < nums.length; end++)
-        {
-            sum += nums[end];
-            while((sum * (end-start+1)) >= k)
-            {
-                sum -= nums[start];
-                start++;
-            }
-            if((sum * (end-start+1)) < k)
-                count+=(end-start+1);
+    int s = 0;
+    int e = 0;
+    long sum1 = 0;
+    long sum2 = 0;
+    long ans = 0;
+    while (e < nums.length) {
+        sum1 = sum1 + nums[e];
+        sum2 = sum1 * (e - s + 1);
+        while (sum2 >= k) {
+            sum1 = sum1 - nums[s];
+            sum2 = sum1 * (e - s);
+            s++;
         }
-        return count;
+        
+        ans = ans + (e - s + 1); // Increment ans by (e - s + 1) here.
+        e++;
     }
+    
+    return ans;
+}
+
 }
