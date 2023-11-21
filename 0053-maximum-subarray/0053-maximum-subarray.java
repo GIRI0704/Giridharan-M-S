6ff6cc6c3c882fd38a1258a4eed9b3c9d909bbc8
Giridharan M S <134040476+GIRI0704@users.noreply.github.com>
2023-11-21 11:27:43 +0530

    Time: 1 ms (99.99%), Space: 60 MB (11.68%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,14 +1,26 @@
 class Solution {
-    public int maxSubArray(int[] a) {
-        int n = a.length;
-        int train = 0;
-		int maxi = Integer.MIN_VALUE;
-		for(int i = 0;i<n;i++){
-			int newTrain = a[i];
-			int prevTrain = train + a[i];
-			train = Math.max(newTrain,prevTrain);
-			maxi = Math.max(maxi,train);
-		}
-		return maxi;
+    public int maxSubArray(int[] nums) {
+        int start = 0;
+        int end = 0;
+        int maxSum = Integer.MIN_VALUE;
+        int currentSum = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        while (end < nums.length) {
+            currentSum += nums[end];
+
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+
+            if (currentSum < 0) {
+                currentSum = 0;
+                start = end + 1;
+            }
+
+            end++;
+        }
+
+        return maxSum;
     }
 }
