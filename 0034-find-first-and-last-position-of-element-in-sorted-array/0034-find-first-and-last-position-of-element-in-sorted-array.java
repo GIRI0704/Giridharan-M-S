507056110cd8c879c05c0ced5c364dff2651a562
Giridharan M S <134040476+GIRI0704@users.noreply.github.com>
2024-02-24 11:16:50 +0530

    Time: 0 ms (100.00%), Space: 45.9 MB (53.74%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,59 +1,60 @@
 class Solution {
-    public static int firstIndex(int arr[], int n, int k)
-	{
-		int i = 0;
-		int j = n-1;
-		int first = -1;
-		while(i <= j)
-			{
-				int mid = (i+j) / 2;
-				if(arr[mid] == k)
-				{
-					first = mid;
-					j = mid-1;
-				}
-				else if(k > arr[mid])
-				{
-					i = mid+1;
-				}
-				else
-				{
-					j = mid-1;
-				}
-			}
-		return first;
-	}
-
-	public static int lastIndex(int arr[], int n, int k)
-	{
-		int i = 0; 
-		int j = n-1;
-		int last = -1;
-
-		while(i <= j)
-			{
-				int mid = (i+j) / 2;
-				if(arr[mid] == k)
-				{
-					last = mid;
-					i = mid+1;
-				}
-				else if(k > arr[mid])
-				{
-					i = mid+1;
-				}
-				else j = mid-1;
-			}
-		return last;
-	}
     public int[] searchRange(int[] nums, int target) {
-        int n = nums.length;
-        int first = firstIndex(nums,n,target);
-		int last = lastIndex(nums,n,target);
-
-		int ans[] = new int[2];
-        ans[0] = first;
-        ans[1] = last;
+        int ans[] = new int[2];
+        ans[0] = -1;
+        ans[1] = -1;
+        if(nums.length == 0) return ans;
+        int left = smaller(nums,target);
+        int right = larger(nums,target);
+        
+        ans[0] = left;
+        ans[1] = right;
+        return ans;
+    }
+    
+    public int smaller(int[] nums, int target)
+    {
+        int start = 0;
+        int end = nums.length-1;
+        int ans = -1;
+        while(start <= end)
+        {
+            int mid = (start+end) / 2;
+            if(nums[mid]==target)
+            {
+                ans = mid;
+                end = mid-1;
+                continue;
+            }
+                
+            if(nums[mid]<target)
+                start = mid+1;
+            else
+                end = mid-1;
+        }
+        return ans;
+    }
+    
+    public int larger(int[] nums, int target)
+    {
+        int start = 0;
+        int end = nums.length-1;
+        int ans = -1;
+        while(start <= end)
+        {
+            int mid = (start+end) / 2;
+            if(nums[mid]==target)
+            {
+                ans = mid;
+                start = mid+1;
+                continue;
+            }
+                
+            if(nums[mid]<target)
+                start = mid+1;
+            else
+                end = mid-1;
+        }
         return ans;
     }
 }
