6b7b872a8269905a830e204d8eac937c8fb220af
Giridharan M S <134040476+GIRI0704@users.noreply.github.com>
2024-05-24 00:07:46 +0530

    Time: 370 ms (5.04%), Space: 45.3 MB (33.58%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,51 +1,42 @@
 class Solution {
-    public boolean freqcheck(HashMap<Character, Integer> f1, HashMap<Character, Integer> f2)
-	{
-		for(char key : f2.keySet())
-			{
-				if(!f1.containsKey(key)) return false;
-				if(f1.get(key) < f2.get(key)) return false;
-			}
-		return true;
-	}
     public String minWindow(String s, String t) {
-        int left = 0;
-		int right = 0;
-		int len = Integer.MAX_VALUE;
-		String ans = "";
+        
+        HashMap<Character, Integer> charMap = new HashMap<>();
+        for(char c: t.toCharArray()) {
+            charMap.put(c, charMap.getOrDefault(c, 0) +1);
+        }
+        int left=0;
+        int n = s.length();
+        int len = Integer.MAX_VALUE;
+        String res = "";
+        HashMap<Character, Integer> currentCharMap = new HashMap<>();
+        for(int right = 0 ; right < n ; right++) {
+            char ch = s.charAt(right);
+            currentCharMap.put(ch, currentCharMap.getOrDefault(ch,0)+1);
+            while(sameMap(currentCharMap, charMap)) {
+                if((right-left)+1 < len) {
+                    len = (right-left)+1;
+                    res = s.substring(left, right+1);
+                }
+                char leftCh = s.charAt(left);
+                currentCharMap.put(leftCh, currentCharMap.getOrDefault(leftCh, 0)-1);
+                if(currentCharMap.get(leftCh) < 1) {
+                    currentCharMap.remove(leftCh);
+                }
+                left++;
+            }
+            
+        }
 
-		HashMap<Character, Integer> f2 = new HashMap<>();
-		HashMap<Character, Integer> f1 = new HashMap<>();
+        return res;
+    }
 
-		for(int i = 0; i < t.length(); i++)
-			{
-				char ch = t.charAt(i);
-				f2.put(ch, f2.getOrDefault(ch,0)+1);
-			}
-
-		while(right < s.length())
-			{
-				char ch = s.charAt(right);
-				f1.put(ch, f1.getOrDefault(ch, 0)+1);
-
-				while(freqcheck(f1,f2))
-					{
-						if(right-left+1 < len)
-						{
-							len = right-left+1;
-							ans = s.substring(left,right+1);
-						}
-
-						char currleft = s.charAt(left);
-						f1.put(currleft, f1.get(currleft)-1);
-						if(f1.get(currleft) == 0)
-						{
-							f1.remove(currleft);
-						}
-						left++;
-					}
-				right++;
-			}
-		return ans;
+    public boolean sameMap(HashMap<Character, Integer> currCharMap, HashMap<Character, Integer> charMap) {
+            for(Character key :  charMap.keySet()) {
+                if( charMap.get(key) > currCharMap.getOrDefault(key, -1)) {
+                    return false;
+                }
+            }
+            return true;
     }
 }
