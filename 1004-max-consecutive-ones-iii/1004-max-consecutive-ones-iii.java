a392e227f0d44fb0abdbc26db5b30c17fd2dad48
Giridharan M S <134040476+GIRI0704@users.noreply.github.com>
2024-05-26 18:54:51 +0530

    Time: 3 ms (84.34%), Space: 46.6 MB (86.73%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -3,18 +3,22 @@ class Solution {
         int start = 0;
         int end = 0;
         int count = 0;
-        int max = 0;
+        int ans = 0;
+        
         while(end < nums.length)
         {
-            if(nums[end] == 0) count++;
+            int num = nums[end];
+            if(num == 0) count++;
+            
             while(count > k)
             {
                 if(nums[start] == 0) count--;
                 start++;
             }
-            max = Math.max(max, end-start+1);
-            end++;
+            
+            ans = Math.max(ans,(end-start)+1);
+        end++;
         }
-        return max;
+        return ans;
     }
 }
