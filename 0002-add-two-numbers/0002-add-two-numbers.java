697c43b5d4e7bf2d36dbf8dce10810d020a8e5d1
Giridharan M S <134040476+GIRI0704@users.noreply.github.com>
2024-05-27 23:12:21 +0530

    Time: 1 ms (100.00%), Space: 44.1 MB (75.61%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -9,70 +9,62 @@
  * }
  */
 class Solution {
-    public ListNode reverse(ListNode head)
-    {
-        ListNode current = head;
-        ListNode next = null;
-        ListNode prev = null;
-        
-        while(current != null)
-        {
-            next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
-        }
-        
-        return prev;
-    }
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode first = l1;
-        
-        ListNode second = l2;
+        ListNode a = l1;//reverse(l1);
+        ListNode b = l2;//reverse(l2);
         
         int carry = 0;
-        
         ListNode head = null;
         ListNode tail = null;
         
-        while(first != null || second != null || carry != 0)
+        while(a != null || b != null || carry != 0)
         {
             int sum = 0;
-            
-            if(first != null)
+            if(a != null)
             {
-                sum = sum+first.val;
-                first = first.next;
+                sum += a.val;
+                a = a.next;
             }
-            
-            if(second != null)
+            if(b != null)
             {
-                sum = sum+second.val;
-                second = second.next;
+                sum += b.val;
+                b = b.next;
             }
             
             int value = sum + carry;
             
-            int lastDigit = value%10;
-            
+            int num = value % 10;
             carry = value / 10;
             
-            ListNode newnode = new ListNode(lastDigit);
+            ListNode newNode = new ListNode(num);
+        
             
             if(head == null)
             {
-                head = newnode;
-                tail = newnode;
+                head = newNode;
+                tail = newNode;
             }
             else
             {
-                tail.next = newnode;
-                tail = newnode;
+                tail.next = newNode;
+                tail = newNode;
             }
         }
         
-        
-        
         return head;
+    }
+    public ListNode reverse(ListNode current)
+    {
+        ListNode next = null;
+        ListNode pre = null;
+        
+        while(current != null)
+        {
+            next = current.next;
+            current.next = pre;
+            pre = current;
+            current = next;
+        }
+        return pre;
     }
 }
